22079b42a3882d0ab2301b63fd5316a35c1cb48c
Daniil Karkozov <dkarkozov@gmail.com>
2023-05-02 10:30:18 +0300

    router

--- echo_bot/bot.py
+++ echo_bot/bot.py
@@ -1,15 +1,24 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
-from config_data.config import load_config, Config
+from config_data.config import Config, load_config
+from handlers import user_handlers, other_handlers
 
 
+# Функция конфигурирования и запуска бота
 async def main() -> None:
+
+    # Загружаем конфиг в переменную config
     config: Config = load_config()
 
+    # Инициализируем бот и диспетчер
     bot: Bot = Bot(token=config.tg_bot.token)
     dp: Dispatcher = Dispatcher()
 
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
